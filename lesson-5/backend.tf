7c677087f604e0a862513460933b4e776a3cb6e2
Mykola Fedotov <113369815+FedotovMS@users.noreply.github.com>
2025-11-22 21:27:49 +0100

    Delete lesson-5 directory

--- lesson-5/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-lesson5-terraform-goit-neovercity-demo-hw5-state"
-    key            = "lesson-5/terraform.tfstate"
-    region         = "eu-north-1"                        
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
