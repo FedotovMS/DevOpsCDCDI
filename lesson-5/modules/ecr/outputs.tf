1431c74eb81bd7a8bccacfffe43e8d7b34dc1fd6
Mykola Fedotov <fedotovns@gmail.com>
2025-11-22 21:14:25 +0100

    Remove everything except lesson-8-9

--- lesson-5/modules/ecr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "repository_url" {
-  value       = aws_ecr_repository.this.repository_url
-  description = "Full ECR repo URL (account.dkr.ecr.region.amazonaws.com/repo)"
-}
-
-output "repository_arn" {
-  value       = aws_ecr_repository.this.arn
-  description = "ECR repository ARN"
-}
--- lesson-7/charts/django-app/templates/_helpers.tpl
+++
@@ -1,7 +0,0 @@
-{{- define "django-app.name" -}}
-{{ .Chart.Name }}
-{{- end }}
-
-{{- define "django-app.fullname" -}}
-{{ .Release.Name }}-{{ .Chart.Name }}
-{{- end }}
